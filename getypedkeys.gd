f8f1de6b77dccff6cdc5a39f58dd2c159ca82aae
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-04-11 11:22:08 -0400

    Add input handling according to 'Outline' (with placeholder for cursor)

--- getypedkeys.gd
+++ getypedkeys.gd
@@ -1,16 +1,55 @@
 extends RichTextLabel
 
+@export var reject_invalid_characters : bool
+@export var pattern : String
+var _cursor_location = 0
+
+enum CursorBackground {
+	EMPTY,
+	CORRECT,
+	ERROR
+}
+
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_BACKSPACE:
-			text = text.erase(text.length() - 1)
+			if _cursor_location > 0:
+				_set_cursor_background(CursorBackground.EMPTY)
+				_cursor_location -= 1
+			else:
+				print("invalid backspace")
+				# later: shake label on invalid input
 		elif event.keycode >= KEY_A and event.keycode <= KEY_Z:
-			append_text(OS.get_keycode_string(event.keycode))
+			if _cursor_location < pattern.length():
+				var letter = OS.get_keycode_string(event.keycode).to_lower()
+				if pattern[_cursor_location] == letter:
+					_set_cursor_background(CursorBackground.CORRECT)
+					_cursor_location += 1
+				else:
+					_set_cursor_background(CursorBackground.ERROR)
+					if not reject_invalid_characters:
+						_cursor_location += 1
+			else:
+				print("reached end of pattern")
+				# later: shake label on invalid input				
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+	# setup bbcode
+	# loop through pattern and wrap each character in [bgcolor=EMPTY]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _set_cursor_background(cursor_background):
+	var color : String
+	match cursor_background:
+		CursorBackground.EMPTY:
+			color = "grey"
+		CursorBackground.CORRECT:
+			color = "green"
+		CursorBackground.ERROR:
+			color = "red"
+	print(color)
