fce0b1111023454be4b613079f71ff300e499f1c
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-04-11 10:37:08 -0400

    Use append_text so it won't delete formatting

--- getypedkeys.gd
+++ getypedkeys.gd
@@ -3,16 +3,13 @@ extends RichTextLabel
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_BACKSPACE:
-			print(text.length())			
-			set_text(text.erase(text.length() - 1))
-			print(text.length())
+			text = text.erase(text.length() - 1)
 		elif event.keycode >= KEY_A and event.keycode <= KEY_Z:
-			text += OS.get_keycode_string(event.keycode)
+			append_text(OS.get_keycode_string(event.keycode))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
