fe43f5d0d86d74ec829e9cf609d7cf63c471958f
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-04-12 11:03:29 -0400

    User can see results of their typing now
    * Modified backspace so it erases the correct character
    * Added is_complete() function to check if pattern is correct
    * Correct/incorrect typing updates pattern background

--- getypedkeys.gd
+++ getypedkeys.gd
@@ -2,54 +2,71 @@ extends RichTextLabel
 
 @export var reject_invalid_characters : bool
 @export var pattern : String
-var _cursor_location = 0
+var _character_backgrounds : Array[CharacterBackground]
+var _cursor_position = 0
 
-enum CursorBackground {
+enum CharacterBackground {
 	EMPTY,
 	CORRECT,
 	ERROR
 }
 
+func is_complete():
+	return _character_backgrounds.all(func(bg): return bg == CharacterBackground.CORRECT)
+
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_BACKSPACE:
-			if _cursor_location > 0:
-				_set_cursor_background(CursorBackground.EMPTY)
-				_cursor_location -= 1
+			if _cursor_position > 0:
+				_cursor_position -= 1
+				_set_cursor_background(CharacterBackground.EMPTY)
 			else:
 				print("invalid backspace")
 				# later: shake label on invalid input
 		elif event.keycode >= KEY_A and event.keycode <= KEY_Z:
-			if _cursor_location < pattern.length():
+			if _cursor_position < pattern.length():
 				var letter = OS.get_keycode_string(event.keycode).to_lower()
-				if pattern[_cursor_location] == letter:
-					_set_cursor_background(CursorBackground.CORRECT)
-					_cursor_location += 1
+				if pattern[_cursor_position] == letter:
+					_set_cursor_background(CharacterBackground.CORRECT)
+					_cursor_position += 1
 				else:
-					_set_cursor_background(CursorBackground.ERROR)
+					_set_cursor_background(CharacterBackground.ERROR)
 					if not reject_invalid_characters:
-						_cursor_location += 1
+						_cursor_position += 1
 			else:
 				print("reached end of pattern")
-				# later: shake label on invalid input				
+				# later: shake label on invalid input
+		print(is_complete())
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	# setup bbcode
-	# loop through pattern and wrap each character in [bgcolor=EMPTY]
+	for character in pattern:
+		_character_backgrounds.append(CharacterBackground.EMPTY)
+	_redraw()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _set_cursor_background(cursor_background):
-	var color : String
+func _character_background_to_color(cursor_background):
 	match cursor_background:
-		CursorBackground.EMPTY:
-			color = "grey"
-		CursorBackground.CORRECT:
-			color = "green"
-		CursorBackground.ERROR:
-			color = "red"
-	print(color)
+		CharacterBackground.EMPTY:
+			return "grey"
+		CharacterBackground.CORRECT:
+			return "green"
+		CharacterBackground.ERROR:
+			return "red"
+
+func _set_cursor_background(cursor_background):
+	_character_backgrounds[_cursor_position] = cursor_background
+	_redraw()
+
+func _redraw():
+	text = ""
+	text = "[center]" # there is no push_center()
+	push_mono()
+	assert(_character_backgrounds.size() == pattern.length())
+	for i in range(_character_backgrounds.size()):
+		push_bgcolor(_character_background_to_color(_character_backgrounds[i]))
+		append_text(pattern[i])
+		pop()
